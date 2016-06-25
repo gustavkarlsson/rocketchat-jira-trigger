957b279b12d297fd19077609d8f22adb8db98f0d
Gustav Karlsson <gustav.karlsson@gmail.com>
2016-06-25 23:09:07 +0200

    Added jacoco plugin and a simple test

---
+++ src/test/java/se/gustavkarlsson/rocketchat/jira_trigger/models/OutgoingMessageTest.java
@@ -0,0 +1,11 @@
+package se.gustavkarlsson.rocketchat.jira_trigger.models;
+
+import org.junit.Test;
+
+public class OutgoingMessageTest {
+
+	@Test
+	public void constructEmpty() throws Exception {
+		new OutgoingMessageTest();
+	}
+}
