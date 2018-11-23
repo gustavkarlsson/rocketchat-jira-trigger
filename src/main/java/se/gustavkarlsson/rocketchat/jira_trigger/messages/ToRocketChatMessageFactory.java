340f44a5757e69076794dabbd4613c4470ae7f11
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-11-23 22:21:34 +0100

    Updated to match latest models

--- src/main/java/se/gustavkarlsson/rocketchat/jira_trigger/messages/ToRocketChatMessageFactory.java
+++ src/main/java/se/gustavkarlsson/rocketchat/jira_trigger/messages/ToRocketChatMessageFactory.java
@@ -4,18 +4,18 @@ import se.gustavkarlsson.rocketchat.models.to_rocket_chat.ToRocketChatMessage;
 
 public class ToRocketChatMessageFactory {
 
-	private final String username;
-	private final String iconUrl;
+	private final String alias;
+	private final String avatar;
 
-	ToRocketChatMessageFactory(String username, String iconUrl) {
-		this.username = username;
-		this.iconUrl = iconUrl;
+	ToRocketChatMessageFactory(String alias, String avatar) {
+		this.alias = alias;
+		this.avatar = avatar;
 	}
 
 	public ToRocketChatMessage create() {
 		ToRocketChatMessage message = new ToRocketChatMessage();
-		message.setAlias(username);
-		message.setAvatar(iconUrl);
+		message.setAlias(alias);
+		message.setAvatar(avatar);
 		return message;
 	}
 
